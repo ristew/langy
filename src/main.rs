b7ed766d72863701713cf974fcb0566704cc72db
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2015-08-25 20:51:23 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::env;
+
+fn main() {
+    let mut arguments = env::args();
+    arguments.next();
+    match arguments.next() {
+        Some(fname) => println!("{}", read_file(fname)),
+        None => println!("no file"),
+    }
+}
+
+fn read_file(fname: String) -> String {
+    use std::io::prelude::*;
+    use std::fs::File;
+    let mut f = File::open(fname).unwrap();
+    let mut res = String::new();
+    f.read_to_string(&mut res).unwrap();
+    res
+}
